fd36c473c3c8d75158af0c654a049f2224e7acff
Vedh Reddy <vedhkreddy@gmail.com>
2021-06-30 11:25:48 -0700

    finished wk2 day 1 p1

---
+++ Bronze/Week2/Day1/square.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ifstream fin("square.in");
+    ofstream fout("square.out");
+    pair<pair<int, int>, pair<int, int>> first;
+    pair<pair<int, int>, pair<int, int>> second;
+    int a, b, c, d;
+    fin >> a >> b >> c >> d;
+    first.first.first = a;
+    first.first.second = b;
+    first.second.first = c;
+    first.second.second = d;
+    int x, y, z, v;
+    fin >> x >> y >> z >> v;
+    second.first.first = x;
+    second.first.second = y;
+    second.second.first = z;
+    second.second.second = v;
+    int xminmax = (max({second.first.first, second.second.first, first.first.first, first.second.first}) - min({second.first.first, second.second.first, first.first.first, first.second.first}));
+    int yminmax = (max({second.second.second, second.first.second, first.first.second, first.second.second}) - min({second.second.second, second.first.second, first.first.second, first.second.second}));
+    if(xminmax >= yminmax){
+        fout << pow(xminmax, 2);
+    }
+    else{
+        fout << pow(yminmax, 2);
+    }
+}
