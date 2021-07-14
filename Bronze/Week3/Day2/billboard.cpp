e6b0f92b45e2f607d1c986425930f96a5a6d7bf7
Vedh Reddy <vedhkreddy@gmail.com>
2021-07-14 16:49:04 -0700

    10/10optimized:billboard

--- Bronze/Week3/Day2/billboard.cpp
+++ Bronze/Week3/Day2/billboard.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 int main(){
+    //usaco.org/index.php?page=viewproblem2&cpid=759
     ifstream fin("billboard.in");
     ofstream fout("billboard.out");
     int a, b, c, d, e, f, g, h, i, j, k, l;
@@ -16,26 +17,25 @@ int main(){
     vector<int> second = {e, f, g, h};
     vector<int> truck = {i, j, k, l};
     set<pair<int, int>> squares_covered_billboards;
-    for(int i = first[0]; i < first[2]; i++){
-        for(int j = first[1]; j < first[3]; j++){
-            squares_covered_billboards.insert(make_pair(i, j));
-        }
-    }
+    int count = (first[3] - first[1]) * (first[2] - first[0]) + (second[3] - second[1]) * (second[2] - second[0]);
     for(int i = second[0]; i < second[2]; i++){
         for(int j = second[1]; j < second[3]; j++){
-            squares_covered_billboards.insert(make_pair(i, j));
-        }
-    }
-    int count = squares_covered_billboards.size();
-    for(int i = truck[0]; i < truck[2]; i++){
-        for(int j = truck[1]; j < truck[3]; j++){
-            set<pair<int, int>> s = squares_covered_billboards;
-            int size = s.size();
-            s.insert(make_pair(i, j));
-            if (size == s.size()){
+            if (i >= first[0] && i < first[2] && j >= first[1] && j < first[3]){
                 count--;
             }
         }
     } 
+    for(int i = truck[0]; i < truck[2]; i++){
+        for(int j = truck[1]; j < truck[3]; j++){
+            if (i >= first[0] && i < first[2] && j >= first[1] && j < first[3]){
+                count--;
+            }
+            else if (i >= second[0] && i < second[2] && j >= second[1] && j < second[3]){
+                count--;
+            }
+            else{}
+        }
+    } 
     fout << count << endl;
 }
+
